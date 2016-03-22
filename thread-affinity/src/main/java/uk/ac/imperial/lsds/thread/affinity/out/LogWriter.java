fd46b3de4f2634ffd3c82c98264c9d6e54ff626b
Panagiotis Garefalakis <pangaref@gmail.com>
2016-03-22 19:26:15 +0000

    Custom Histogram Log writer

---
+++ thread-affinity/src/main/java/uk/ac/imperial/lsds/thread/affinity/out/LogWriter.java
@@ -0,0 +1,59 @@
+package uk.ac.imperial.lsds.thread.affinity.out;
+
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+
+import org.HdrHistogram.Histogram;
+import org.HdrHistogram.HistogramLogWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.imperial.lsds.thread.affinity.Utils;
+
+
+/**
+ * @author pg1712
+ *
+ */
+public class LogWriter {
+	
+	private String filename;
+	private String path;
+	private HistogramLogWriter logWriter;
+	private static Logger LOG = LoggerFactory.getLogger(LogWriter.class.getName());
+	
+	public LogWriter(String path, String filename){
+		this.path = Utils.getProjectHome() +"/"+ path +"/";
+		this.filename = filename;
+		
+		try {
+			this.logWriter = new HistogramLogWriter(this.path+ this.filename);
+		} catch (FileNotFoundException e) {
+			LOG.error("Could not persist HDR log {} ", filename);
+			LOG.error("Exception {}", e);
+		}
+		// headers etc.
+		this.setup();
+	}
+	
+	public void setup(){
+		// Some log header bits
+		this.logWriter.outputComment("[Logged with " + LogWriter.class.getName() + "]");
+		this.logWriter.outputLogFormatVersion();
+		this.logWriter.outputStartTime(System.currentTimeMillis());
+		this.logWriter.setBaseTime(System.currentTimeMillis());
+		
+	}
+	
+	public void histoDump(Histogram histo){
+		LOG.info("Persisting histo file {} path {}", this.filename, this.path);
+		// Every once in a while, we just drop some histograms
+		this.logWriter.outputComment("[ Custom Summary Average 90th 99th \t"+
+				histo.getMean()/1000000.0 + "\t"+
+				histo.getValueAtPercentile(90)/1000000.0 + "\t"+
+				histo.getValueAtPercentile(99)/1000000.0 + "]");
+		this.logWriter.outputLegend();
+		this.logWriter.outputIntervalHistogram(histo);
+	}
+
+}
