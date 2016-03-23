63e94f1cca424362b87f00f8dca6f3bbacc1d107
Panagiotis Garefalakis <pangaref@gmail.com>
2016-03-23 11:06:15 +0000

    Adding more experiment parameters

--- thread-affinity/src/main/java/uk/ac/imperial/lsds/thread/affinity/Utils.java
+++ thread-affinity/src/main/java/uk/ac/imperial/lsds/thread/affinity/Utils.java
@@ -8,9 +8,11 @@ public class Utils {
 	
 	static String PROJECT_HOME;
 	/* Experiments conf */
-	static boolean CORE_PIN=false;
-	static boolean HYPER_THREAD=true;
-	static int WORKER_THREADS = 4;
+	static boolean CORE_PIN = true;
+	static boolean HYPER_THREAD = false;
+	static int WORKER_THREADS = 1;
+	static int FIBONACCI_NUMBER = 20;
+	static int EXPERIMENT_RUNTIME = 60; // in seconds
 	/* Up to here */
 	
 	public static String getProjectHome(){
