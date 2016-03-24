5db60af45f80aa68091a4076486fe5febb543e23
Panagiotis Garefalakis <pangaref@gmail.com>
2016-03-24 18:14:29 +0000

    Multithread application gradualy increasing the number of Fib workers - using the configuration provided in the utils

--- thread-affinity/src/main/java/uk/ac/imperial/lsds/thread/affinity/SimpleMultithreadApp.java
+++ thread-affinity/src/main/java/uk/ac/imperial/lsds/thread/affinity/SimpleMultithreadApp.java
@@ -1,10 +1,21 @@
 package uk.ac.imperial.lsds.thread.affinity;
 
-import java.util.concurrent.Executor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import org.HdrHistogram.Histogram;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import uk.ac.imperial.lsds.thread.affinity.out.LogWriter;
 
 /**
  * Hello world!
@@ -14,27 +25,80 @@ public class SimpleMultithreadApp {
 	
 	private static int threads = Utils.WORKER_THREADS;
 	private static int totalCores = 0;
-
-    private static Logger LOG = LoggerFactory.getLogger(SimpleMultithreadApp.class.getName());
+	private List<SimpleFibProcessor> runnables;
+	private static Logger LOG = LoggerFactory.getLogger(SimpleMultithreadApp.class.getName());
+	private  ExecutorService excService;
 	
-	public static void main( String[] args ){
-		
-		/* Use HyperThreads or Not */
+	public SimpleMultithreadApp(){
+		runnables = new ArrayList<SimpleFibProcessor>();
+	}
+	
+    private void setup(){
+    	/* Use HyperThreads or Not */
 		if(Utils.HYPER_THREAD)
 			totalCores = TheCPU.getInstance().getNumCores();
 		else
 			totalCores = TheCPU.getInstance().getNumCores()/2;
+		LOG.info("=== CORE Pin: {} HyperThread: {} Total Cores: {} Workers: {} ===",
+				Utils.CORE_PIN, Utils.HYPER_THREAD , SimpleMultithreadApp.totalCores, SimpleMultithreadApp.threads);
 		
-		LOG.info("=== CORE Pin: {} HyperThread: {} Total Cores: {} Workers: {} ===",Utils.CORE_PIN, Utils.HYPER_THREAD , totalCores, Utils.WORKER_THREADS);
-		
-		LOG.info( "Starting Main Application..." );
-		Executor exc = Executors.newFixedThreadPool(threads);
-		
+		LOG.debug( "Setting up Application..." );
+		excService = Executors.newFixedThreadPool(threads);
 		for(int i =0 ;i < threads; i++){
-			LOG.info("Processor: "+ i);
-			exc.execute( new SimpleFibProcessor(i%totalCores, Utils.CORE_PIN) );
+			LOG.debug("Processor: "+ i);
+			SimpleFibProcessor newFib =  new SimpleFibProcessor(i%totalCores, Utils.CORE_PIN);
+			this.runnables.add( newFib );
+			excService.execute( newFib );
+		}		
+        LOG.debug("Setup Done!");
+	}
+    
+    public void run_experiment(){
+    	long start = System.currentTimeMillis();
+		while((System.currentTimeMillis() - start) < (Utils.EXPERIMENT_RUNTIME*1000) ){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-        
-        LOG.info("Startup Done!");
+		Histogram allLatencies = new Histogram(TimeUnit.SECONDS.toNanos(10), 3);
+		for(SimpleFibProcessor tmp : this.runnables){
+			tmp.stopWorking();
+			allLatencies.add(tmp.getLatencies());
+			LOG.debug("Thread working: {} Stats Size in Bytes: {}", tmp.isWorking(), tmp.getLatencies().getEstimatedFootprintInBytes());
+		}
+		this.excService.shutdown();
+		
+		LOG.debug("Summary Stats Size: {}", allLatencies.getEstimatedFootprintInBytes());
+		//Latency in X divide nanos by ?? => Miliseconds are enough for this experiment
+		//allLatencies.outputPercentileDistribution(System.out, 1000000.0);
+		LOG.info("Workers {} Summary Average 90th 99th {}\t{}\t{}",
+				SimpleMultithreadApp.threads,
+				allLatencies.getMean()/1000000.0,
+				allLatencies.getValueAtPercentile(90)/1000000.0,
+				allLatencies.getValueAtPercentile(99)/1000000.0);
+		
+		LogWriter histoWrite  = new LogWriter("data", 
+				"Fib_" + Integer.toString(Utils.FIBONACCI_NUMBER)
+				+"_Pin-" + Utils.CORE_PIN
+				+"_Workers-" + SimpleMultithreadApp.threads
+				+"_Cores-" + totalCores+".log");
+		histoWrite.histoDump(allLatencies);
+    }
+    
+    
+    
+	public static void main( String[] args ){
+		SimpleMultithreadApp app = new SimpleMultithreadApp();
+		
+		for (int i = 1; i < 11; i++){
+			SimpleMultithreadApp.threads = i;
+			app.setup();
+			app.run_experiment();
+		}
+		LOG.info("All Done!!");
+	
     }
 }
